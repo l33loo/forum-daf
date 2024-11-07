87ecb1bec78ba23ede5b875e27b4c9f40ffe62cb
Lila Karpowicz <lila@karpowicz.dev>
2024-11-07 16:26:41 -0100

    add bash script for docker

---
+++ docker-compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# lists Symfony commands
+# docker compose run --rm php bin/console
+# docker compose run --rm php bin/console doctrine:migrations:migrate
+docker compose down
+docker compose up -d --build
+# docker compose run -it --rm -v "$(pwd):/var/www/app" php gosu www-data composer install
+# docker compose run -u 0 -it --rm php gosu www-data composer install
+docker compose run -it --rm php gosu www-data composer install
