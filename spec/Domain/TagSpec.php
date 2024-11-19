9f02ce4a66271951f8a06ea3923e98ce2d8d2d1b
Lila Karpowicz <lila@karpowicz.dev>
2024-11-19 11:28:10 -0100

    make Tag even generator, and add Doctrine attributes

--- spec/Domain/TagSpec.php
+++ spec/Domain/TagSpec.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace spec\App\Domain;
 
 use App\Domain\Tag;
-
+use App\Domain\Event\Tag\TagWasCreated;
 use PhpSpec\ObjectBehavior;
+use Slick\Event\EventGenerator;
+
 class TagSpec extends ObjectBehavior
 {
     private $tag;
@@ -25,5 +27,13 @@ class TagSpec extends ObjectBehavior
     function it_has_a_tag(): void
     {
         $this->tag()->shouldBe($this->tag);
+    }
+
+    function its_an_event_generator()
+    {
+        $this->shouldBeAnInstanceOf(EventGenerator::class);
+        $events = $this->releaseEvents();
+        $events->shouldHaveCount(1);
+        $events[0]->shouldBeAnInstanceOf(TagWasCreated::class);
     }
 }
