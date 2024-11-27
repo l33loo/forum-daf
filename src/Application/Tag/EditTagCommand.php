fa08593f7d85487d2cc6116c492dd2f761d111f7
Lila Karpowicz <lila@karpowicz.dev>
2024-11-27 11:47:37 -0100

    edit tag command

---
+++ src/Application/Tag/EditTagCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Tag;
+
+use App\Domain\Tag\TagId;
+use Slick\JSONAPI\Object\SchemaDiscover\Attributes\AsResourceObject;
+
+#[AsResourceObject('tags')]
+final class EditTagCommand
+{
+    public function __construct(
+        private TagId $tagId,
+        private string $tag
+    ) {}
+
+    public function tagId()
+    {
+        return $this->tagId;
+    }
+
+    public function tag()
+    {
+        return $this->tag;
+    }
+}
