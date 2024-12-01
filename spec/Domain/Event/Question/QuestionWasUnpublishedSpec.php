6a99658c42b9f626e9dbbd71270e774ab810e7c5
Lila Karpowicz <lila@karpowicz.dev>
2024-12-01 20:42:12 -0100

    question was unpublished

---
+++ spec/Domain/Event/Question/QuestionWasUnpublishedSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\App\Domain\Event\Question;
+
+use App\Domain\Event\Question\QuestionWasUnpublished;
+use App\Domain\Question\QuestionId;
+use DateTimeImmutable;
+use PhpSpec\ObjectBehavior;
+use Slick\Event\Domain\AbstractEvent;
+use Slick\Event\Event;
+
+/**
+ * QuestionWasUnpublishedSpec specs
+ *
+ * @package spec\App\Domain\Event\Question
+ */
+class QuestionWasUnpublishedSpec extends ObjectBehavior
+{
+
+    private $questionId;
+
+    function let()
+    {
+        $this->questionId = new QuestionId();
+        $this->beConstructedWith($this->questionId);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(QuestionWasUnpublished::class);
+    }
+
+    function it_has_a_questionId()
+    {
+        $this->questionId()->shouldBe($this->questionId);
+    }
+
+    function its_an_event()
+    {
+        $this->shouldBeAnInstanceOf(Event::class);
+        $this->shouldHaveType(AbstractEvent::class);
+        $this->occurredOn()->shouldBeAnInstanceOf(DateTimeImmutable::class);
+    }
+
+    function it_can_be_converted_to_json()
+    {
+        $this->shouldBeAnInstanceOf(\JsonSerializable::class);
+        $this->jsonSerialize()->shouldBe([
+            'questionId' => $this->questionId,
+        ]);
+    }
+}
---
+++ src/Application/Listeners/UnpublishQuestionListener.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Listeners;
+
+use App\Application\Question\UnpublishQuestionCommand;
+use App\Application\Question\UnpublishQuestionHandler;
+use App\Domain\Event\Question\QuestionWasAccepted;
+use App\Domain\Event\Question\QuestionWasRejected;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+/**
+ * UnpublishQuestionListener
+ *
+ * @package App\Application\Listeners
+ */
+#[AsEventListener(event: QuestionWasAccepted::class, method: 'onQuestionWasRejected')]
+final readonly class UnpublishQuestionListener
+{
+
+    public function __construct(private UnpublishQuestionHandler $handler)
+    {
+    }
+
+
+    public function onQuestionWasAccepted(QuestionWasRejected $event): void
+    {
+        $this->handler->handle(new UnpublishQuestionCommand($event->questionId()));
+    }
+}
