b08afde2ce86eb914ff06427a4e5fbf09eb2c1c8
Lila Karpowicz <lila@karpowicz.dev>
2024-12-01 20:26:05 -0100

    change question controller

---
+++ src/UserInterface/Api/Question/ChangeQuestionController.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\UserInterface\Api\Question;
+
+use App\Application\Question\ChangeQuestionCommand;
+use App\Application\Question\ChangeQuestionHandler;
+use App\Domain\DomainException;
+use App\Domain\Question\QuestionId;
+use App\Domain\User;
+use App\UserInterface\Api\ApiControllerMethods;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * ChangeQuestionController
+ *
+ * @package App\UserInterface\Api\Question
+ */
+final class ChangeQuestionController extends AbstractController
+{
+    use ApiControllerMethods;
+
+    public function __construct(private readonly ChangeQuestionHandler $handler)
+    {}
+
+    /**
+     * @throws DomainException
+     */
+    #[Route(path: '/api/question/{questionId}', name: 'api-change-question', methods: ['PATCH', 'PUT'])]
+    #[IsGranted(User::ROLE_USER)]
+    public function handle(QuestionId $questionId): Response
+    {
+        $command = $this->decodeTo(ChangeQuestionCommand::class);
+        $question = $this->handler->handle($command);
+        return $this->apiResponse(
+            $question,
+            Response::HTTP_NO_CONTENT,
+            [
+                "location" => $this->generateUrl('api-read-question', ['questionId' => $question->question()])
+            ]
+        );
+    }
+}
