6429e5955a9c09e26b958b215b31ad7f4cd50b39
Lila Karpowicz <lila@karpowicz.dev>
2024-12-02 02:02:33 -0100

    change answer spec

---
+++ spec/Application/Answer/ChangeAnswerHandlerSpec.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\App\Application\Answer;
+
+use App\Application\Answer\ChangeAnswerCommand;
+use App\Application\Answer\ChangeAnswerHandler;
+use App\Domain\Answer;
+use App\Domain\Answer\AnswerRepository;
+use App\Domain\User;
+use App\Domain\User\Email;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Slick\Event\EventDispatcher;
+
+/**
+ * ChangeAnswerHandlerSpec specs
+ *
+ * @package spec\App\Application\Answer
+ */
+class ChangeAnswerHandlerSpec extends ObjectBehavior
+{
+    private Answer $answer;
+
+    function let(
+        AnswerRepository $answers,
+        EventDispatcher $dispatcher
+    ) {
+        $user = new User(new Email('user@email.com'));
+        $this->answer = new Answer($user, "Body...");
+        $answers->withId($this->answer->answerId())->willReturn($this->answer);
+        $dispatcher->dispatchEventsFrom(Argument::type(Answer::class))->willReturn([]);
+
+        $this->beConstructedWith($answers, $dispatcher);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ChangeAnswerHandler::class);
+    }
+
+    function it_handles_change_answer_command(
+        EventDispatcher $dispatcher
+    ) {
+        $command = new ChangeAnswerCommand(
+            $this->answer->answerId(),
+            "Changed body..."
+        );
+        $this->handle($command)->shouldBe($this->answer);
+        $dispatcher->dispatchEventsFrom($this->answer)->shouldHaveBeenCalled();
+    }
+}
