44bb9417e9acebca76ea97fa73b713d02f7750f0
Lila Karpowicz <lila@karpowicz.dev>
2024-12-02 01:58:07 -0100

    change answer command

---
+++ src/Application/Answer/ChangeAnswerCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Answer;
+
+use App\Domain\Answer\AnswerId;
+use Slick\JSONAPI\Object\SchemaDiscover\Attributes\AsResourceObject;
+
+#[AsResourceObject(type: "answers")]
+final class ChangeAnswerCommand
+{
+    public function __construct(
+        private AnswerId $answerId,
+        private string $body
+    ) {}
+
+    public function answerId(): AnswerId
+    {
+        return $this->answerId;
+    }
+
+    public function answer(): string
+    {
+        return $this->answer;
+    }
+
+    public function body(): string
+    {
+        return $this->body;
+    }
+}
